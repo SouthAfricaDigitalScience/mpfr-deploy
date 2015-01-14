17dac538d99ba8c706d152967b36648c84cd1a98
Bruce Becker <brucellino@gmail.com>
2015-01-14 16:04:32 +0100

    checking some variables

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 SOURCE_FILE=$NAME-$VERSION.tar.gz
+echo "we'll now build $NAME-$VERSION" from $SOURCE_FILE
 # Add the module for the CI environment
 module load ci
 
@@ -10,6 +11,9 @@ if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
   echo "Seems we are building from scratch - preparing "
   mkdir -p $SRC_DIR
   wget http://mirror.ufs.ac.za/gnu/gnu/mpfr/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+else
+  echo "Seems we already have that source file $SRC_DIR/$SOURCE_FILE"
+  unpacking from here.
 fi
 tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 cd $WORKSPACE/$NAME-$VERSION
