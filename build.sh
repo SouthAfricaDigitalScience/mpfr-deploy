058113dc5dd2021872a7a18a039a1a7e03644a51
Bruce Becker <brucellino@gmail.com>
2015-01-14 16:07:37 +0100

    typo in build script

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if [[ ! -e $SRC_DIR/$SOURCE_FILE ]] ; then
   wget http://mirror.ufs.ac.za/gnu/gnu/mpfr/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
 else
   echo "Seems we already have that source file $SRC_DIR/$SOURCE_FILE"
-  unpacking from here.
+  echo "unpacking from here."
 fi
 tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
 cd $WORKSPACE/$NAME-$VERSION
