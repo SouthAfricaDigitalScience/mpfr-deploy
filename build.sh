7b64e4d4b2c736e4faaf9f87c3d67a38eb2547c0
Bruce Becker <brucellino@gmail.com>
2015-11-22 18:10:00 +0200

    fixed empty file checking, using --with-gmp and edited for style

--- build.sh
+++ build.sh
@@ -1,22 +1,24 @@
 #!/bin/bash -e
 . /etc/profile.d/modules.sh
-SOURCE_FILE=$NAME-$VERSION.tar.gz
-echo "we'll now build $NAME-$VERSION" from $SOURCE_FILE
 # Add the module for the CI environment
 module load ci
+SOURCE_FILE=${NAME}-${VERSION}.tar.gz
+echo "we'll now build ${NAME}-${VERSION} from ${SOURCE_FILE}"
 
 # add dependencies
-module load gmp/5.1.3
+module add gmp/5.1.3
+echo "GCC Include path is now : "
+echo ${GCC_INCLUDE_DIR}
 
-if [[ ! -s $SRC_DIR/$SOURCE_FILE ]] ; then
+if [[ -s ${SRC_DIR/$SOURCE_FILE} ]] ; then
   echo "Seems we are building from scratch - preparing "
-  mkdir -p $SRC_DIR
-  wget http://mirror.ufs.ac.za/gnu/gnu/mpfr/$SOURCE_FILE -O $SRC_DIR/$SOURCE_FILE
+  mkdir -p ${SRC_DIR}
+  wget http://mirror.ufs.ac.za/gnu/gnu/mpfr/${SOURCE_FILE} -O ${SRC_DIR/}${SOURCE_FILE}
 else
   echo "Seems we already have that source file $SRC_DIR/$SOURCE_FILE"
   echo "unpacking from here."
 fi
-tar -xvzf $SRC_DIR/$SOURCE_FILE -C $WORKSPACE
-cd $WORKSPACE/$NAME-$VERSION
-./configure --prefix $SOFT_DIR 
+tar -xvzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE}
+cd ${WORKSPACE}/${NAME}-${VERSION}
+./configure  --with-gmp=${GMP_DIR}--prefix ${SOFT_DIR}
 make -j 8
